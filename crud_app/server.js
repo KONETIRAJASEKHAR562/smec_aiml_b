36304520bfd82c6ef0877fb1e7efe4a717cdcd99
rajasekhar <konetirajasekhar@gmail.com>
2026-09-17 05:57:26 +0000

    sep 17, server.js files code updated

--- crud_app/server.js
+++ crud_app/server.js
@@ -1,1 +1,23 @@
-console.log("server file is  loaded/opened");
+const express = require('express'); // importing the express module of packages, etc
+const mongoose = require('mongoose'); // same  importing mongodb related 
+const bodyParser = require('body-parser'); // which data format related db
+
+const app = express(); // implementating the server
+app.use(bodyParser.json()); //using the data infor in json
+
+// Connect to local MongoDB Compass instance
+const url = process.env.MONGO_URI || 'mongodb://localhost:27017/crt';
+// here db name is aiml_d, you can change it to your database name
+mongoose.connect(url)
+  .then(() => {
+    const userRoutes = require('./route/userroute');  
+    app.use('/users', userRoutes);
+
+    app.listen(3000, () => console.log('Server is running on port 3000'));
+  })
+  .catch((err) => {
+    console.error("connection failed :: ", err);
+    process.exit(1);
+  });
+
+  
